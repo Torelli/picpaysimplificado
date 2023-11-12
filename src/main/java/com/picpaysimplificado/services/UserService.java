ba8ba934243bc9f8f8404d34eb15dab9d4d24450
Giovanni Torelli <giovanni.walter@outlook.com>
2023-11-12 20:53:59 -0300

    feat: updated UserService

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -24,7 +24,7 @@ public class UserService {
     }
 
     public User findUserById(Long id) throws Exception {
-        return this.repository.findById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
     }
 
     public void saveUser(User user) {
