9bac79054bbc9975641d123e50c6451ff9de04d3
Giovanni Torelli <giovanni.walter@outlook.com>
2023-11-12 21:51:06 -0300

    feat: Updated NotificationService

--- src/main/java/com/picpaysimplificado/service/NotificationService.java
+++ src/main/java/com/picpaysimplificado/service/NotificationService.java
@@ -1,8 +1,7 @@
-package com.picpaysimplificado.services;
+package com.picpaysimplificado.service;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dto.NotificationDTO;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
